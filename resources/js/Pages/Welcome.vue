5f2b7275bbac6a45cd86577183d490b6140a3fb3
raoabdulrehman29-dev <raoabdulrehman29@gmail.com>
2026-05-13 19:43:25 +0500

    new  paragraph added to the Welcome file

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -4,6 +4,7 @@
 <template>
     <div>
         <h1 class="inline-block text-xl font-bold text-gray-500 hover:text-gray-900 underline "> Welcome Home </h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos fuga ea at libero doloremque eum incidunt minima praesentium, numquam explicabo ducimus harum magni quisquam! Culpa ex dolor earum voluptatibus tempora!</p>
     </div>
 </template>
    
